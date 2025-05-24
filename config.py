caaabdf1f8d65e7c44ae40214a11491d856e5e3b
Allenwang2004 <ziyiw1930@gmail.com>
2025-05-24 14:05:24 +0800

    add branch dev

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'dev-secret')  # 預設值供本地測試
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'dev-secret')
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret')
     
     MAIL_SERVER = 'smtp.gmail.com'
