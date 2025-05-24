885a93970b7097fba6854c0479200015e05d53a1
Allenwang2004 <ziyiw1930@gmail.com>
2025-05-24 13:36:42 +0800

    change for render

--- config.py
+++ config.py
@@ -3,14 +3,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://project_user:0927382377@localhost/project_db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'iqwoowodqwq'
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'dev-secret')  # 預設值供本地測試
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret')
+    
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    SECRET_KEY = os.getenv('SECRET_KEY')
+
     ADMIN_EMAIL = os.getenv("ADMIN_EMAIL")
     ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
